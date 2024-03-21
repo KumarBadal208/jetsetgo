cd34d4a88630d1fd6693ae4027fb9aed58ae5a71
Admin <admin@Admins-MacBook-Pro.local>
2024-03-21 23:07:10 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import "./App.css";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div>
-      hello
+      <Home />
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,21 @@
+const Home = ()=>{
+    return (
+        <>
+            <div className="homepage-header">
+                <div>✈︎ ✈️</div>
+            </div>
+            <div className="homepage-body">
+                <div>
+
+                </div>
+            </div>
+            <div className="homepage-footer">
+
+            </div>
+        </>
+    )
+};
+
+export default Home;
+
+// 🔁
